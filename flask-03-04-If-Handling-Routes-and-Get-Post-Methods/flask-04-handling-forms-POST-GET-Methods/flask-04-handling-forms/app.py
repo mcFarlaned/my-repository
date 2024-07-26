c62e54d8ec6c1f485b35e447f1e839a23b3998c1
Devon McFarlane <mcfarlane77@gamil.com>
2024-07-26 12:13:47 -0400

    add flask 04

--- flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
+++ flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-04-handling-forms-POST-GET-Methods/flask-04-handling-forms/app.py
@@ -14,13 +14,13 @@ def welcome():
 # Write a function named `greet` which uses template file named `greet.html` given under 
 # `templates` folder. it takes parameters from query string on URL, assign that parameter 
 # to the 'user' variable and sent that user name into the html file. If it doesn't have any parameter, warning massage is raised
-@app.route('/greet')
+@app.route("/greet")
 def greet():
-    user = request.args.get('user')
-    if user:
-        return render_template('greet.html', user=user)
+    if "user" in request.args:
+        input_user = request.args["user"]
+        return render_template("greet.html", user=input_user)
     else:
-        return "Warning: No user parameter provided."
+        return "No user specified"
 
 
 
@@ -29,8 +29,15 @@ def greet():
 # and assign to the static route of ('login')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Handle login logic here
-    return render_template('login.html')
+    if request.method =="GET":
+        return render_template("login.html", control=False)
+    else:
+        user_name = request.form.get("username")
+        password = request.form.get("password")
+        if password == "clarusway":
+            return render_template("secure.html", user=user_name)
+        else:
+            return render_template("login.html", control=True, user=user_name)
 
 # Add a statement to run the Flask application
 if __name__ == '__main__':
